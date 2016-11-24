8de6f8de2557784abae3eb4f53db2dc83f5756e6
Diego J <djcampos44@gmail.com>
2016-11-24 11:21:58 +0100

    Cambios en string, clase constants

---
+++ app/src/main/java/com/weather/diegojesuscampos/weather/Interfaces/IMyViewHolderClickListener.java
@@ -0,0 +1,11 @@
+package com.weather.diegojesuscampos.weather.Interfaces;
+
+import com.weather.diegojesuscampos.weather.Datos.ObjInfoGeografica;
+
+/**
+ * Created by Diego Jesus Campos on 23/11/2016.
+ */
+
+public interface IMyViewHolderClickListener {
+    void showWeatherPLaces(ObjInfoGeografica infoPlace);
+}
